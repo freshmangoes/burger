11a3559f58732c82b17f6f064c47da508eda5f1a
Kyle C <kylecilia@gmail.com>
2020-01-20 15:11:54 -0800

    Trying to resolve some heroku stuff

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require(`mysql2`);
 
-const connection = mysql.createConnection({
-	host: `localhost`,
-	port: 8889,
-	user: `root`,
-	password: `root`,
-	database: `burger_db`
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: `localhost`,
+		port: 8889,
+		user: `root`,
+		password: `root`,
+		database: `burger_db`
+	});
+}
+
 
 connection.connect((err) => {
 	if (err) return console.error(`error connecting ${err.stack}`);
